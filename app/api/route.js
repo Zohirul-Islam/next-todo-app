48ea331d880193e39a848e4417a832ec876b7101
bijoy <bijoycse24@gmail.com>
2025-10-09 11:59:33 +0600

    add pagination feature

--- app/api/route.js
+++ app/api/route.js
@@ -9,9 +9,27 @@ const db = async()=>{
 db();
 /* getting all todos */
 export async function GET(request){
-
-    const todos = await TodoModel.find({});
-   return NextResponse.json({todos:todos})
+    const { searchParams } = new URL(request.url);
+    const page = Math.max(parseInt(searchParams.get("page")) || 1, 1);
+    const limit = Math.max(parseInt(searchParams.get("limit")) || 5, 1);
+    const skip = (page - 1) * limit;
+    /* get total items and current page data */
+    const [tatalItems,todos] = await Promise.all([
+        TodoModel.countDocuments(),
+        TodoModel.find()
+        .sort({createdAt:-1})
+        .skip(skip)
+        .limit(limit)
+    ])
+    const totalPages = Math.max(Math.ceil(tatalItems / limit), 1);
+    return NextResponse.json({
+      success: true,
+      currentPage: page,
+      totalPages,
+      tatalItems,
+      limit,
+      data: todos,
+    });
 }
 /* sending all todos on database  */
 export async function POST(request){
--- app/page.js
+++ app/page.js
@@ -10,11 +10,22 @@ const Page = () => {
     description: "",
   });
   const [todos,setTodos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const limit = 5; // posts per page
 
-  const fetchTodos = async()=>{
-    const response = await axios.get("/api");
-    console.log(response.data);
-    setTodos(response.data.todos);
+  const fetchTodos = async(pageNumber)=>{
+      try {
+        const res = await fetch(`/api?page=${pageNumber}&limit=${limit}`);
+        const data = await res.json();
+        if (data.success) {
+        setTodos(data.data);
+        setTotalPages(data.totalPages);
+        setPage(data.currentPage)
+      }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
   }
   /* delete todos */
   const deleteTodo =async(mongoId)=>{
@@ -55,8 +66,11 @@ const Page = () => {
     }
   };
   useEffect(()=>{
-    fetchTodos()
-  },[])
+    fetchTodos(page)
+  },[page])
+    // Pagination handlers
+  const handlePrev = () => setPage((p) => Math.max(p - 1, 1));
+  const handleNext = () => setPage((p) => Math.min(p + 1, totalPages));
   return (
     <div className="container mx-auto">
       <ToastContainer theme="dark" />
@@ -121,6 +135,7 @@ const Page = () => {
               </tr>
             </thead>
             <tbody>
+              
               {
                 todos.map((todos,index)=>(
                   <TodoData completeTodo={completeTodo} mongoId ={todos._id} deleteTodo ={deleteTodo} key={index} id={index} title={todos.title} createdAt={todos.createdAt} description ={todos.description} complete = {todos.isCompleted}  />
@@ -129,8 +144,29 @@ const Page = () => {
               
             </tbody>
           </table>
+                {/* Pagination Controls */}
+      <div className="flex justify-center items-center mt-6">
+        <button
+          onClick={handlePrev}
+          disabled={page === 1}
+          className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50 cursor-pointer"
+        >
+          Previous
+        </button>
+        <span>
+          Page {page} of {totalPages}
+        </span>
+        <button
+          onClick={handleNext}
+          disabled={page === totalPages}
+          className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50 cursor-pointer"
+        >
+          Next
+        </button>
+      </div>
         </div>
       </div>
+
     </div>
   );
 };
